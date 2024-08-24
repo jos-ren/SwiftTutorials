253353ac061533526eaf42d4516089c171dbe914
Josh Renema <joshua@territorial.ca>
2024-08-24 12:19:15 -0700

    dice roller

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Joshua Renema on 2024-08-24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
